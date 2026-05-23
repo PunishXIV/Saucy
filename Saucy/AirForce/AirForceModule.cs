6d0e41113188fceb36e40b245117f53e945871ed
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-05-23 23:31:45 +0300

    Update ids

--- Saucy/AirForce/AirForceModule.cs
+++ Saucy/AirForce/AirForceModule.cs
@@ -3,8 +3,10 @@ using Dalamud.Game.ClientState.Objects.SubKinds;
 using ECommons;
 using ECommons.Automation;
 using ECommons.CSExtensions;
+using ECommons.GameFunctions;
 using ECommons.GameHelpers;
 using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using System;
@@ -15,41 +17,43 @@ using System.Text;
 
 namespace Saucy.AirForce;
 
-#pragma warning disable IDE0078
-
 public static unsafe class AirForceModule
 {
     public static void OnUpdate()
     {
         if (Svc.Condition[ConditionFlag.BoundByDuty95] && GenericHelpers.TryGetAddonByName<AtkUnitBase>("RideShooting", out var addon) && addon->IsReady())
         {
-            List<(Vector2 Pos, float radius)> bad = [];
-            foreach (var x in Svc.Objects.OfType<IEventObj>().Where(x => x.BaseId == 2015180 || x.BaseId == 2015179 || x.BaseId == 2015178).Where(x => x.AnimationId == 1).OrderBy(Player.DistanceTo))
+            foreach (var x in Svc.Objects.OfType<IEventObj>().Where(x => x.BaseId.EqualsAny<uint>(
+                2009678,
+                2009676,
+                2009677,
+                2009679,
+                2015180,
+                2015179,
+                2015178,
+                2015183
+                )).Where(x => x.AnimationId == 1).OrderBy(Player.DistanceTo))
             {
+                if (x.BaseId.EqualsAny<uint>(
+                    2015183,
+                    2009679
+                    ))
+                {
+                    continue;
+                }
                 if (Svc.GameGui.WorldToScreen(x.Position, out var screen))
                 {
-                    if (x.BaseId == 2015183)
+                    if (EzThrottler.Throttle("Shoot", 250))
                     {
-                        bad.Add((screen, 4f));
-                    }
-                    else
-                    {
-                        foreach (var b in bad)
+                        var a = *(nint*)((nint)(AgentModule.Instance()->GetAgentByInternalId(AgentId.RideShooting)) + 48);
+                        *(float*)(a + 3184) = screen.X;
+                        *(float*)(a + 3188) = screen.Y;
+                        Svc.Framework.RunOnTick(() =>
                         {
-                            if (Vector2.Distance(b.Pos, screen) < b.radius) continue;
+                            _ = WindowsKeypress.SendKeypress(ECommons.WindowsFormsReflector.Keys.Space);
                         }
-                        if (EzThrottler.Throttle("Shoot", 250))
-                        {
-                            var a = *(nint*)((nint)(AgentModule.Instance()->GetAgentByInternalId(AgentId.RideShooting)) + 48);
-                            *(float*)(a + 3184) = screen.X;
-                            *(float*)(a + 3188) = screen.Y;
-                            Svc.Framework.RunOnTick(() =>
-                            {
-                                _ = WindowsKeypress.SendKeypress(ECommons.WindowsFormsReflector.Keys.Space);
-                            }
-                            , delayTicks: 1);
-                            break;
-                        }
+                        , delayTicks: 1);
+                        break;
                     }
                 }
             }
