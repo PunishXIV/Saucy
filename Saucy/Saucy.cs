6aa2630a9fb659471c0131a52743c7e7e0bb9f75
Knightmore <35805408+Knightmore@users.noreply.github.com>
2026-05-29 12:47:43 +0200

    Removing that chat message dependency

--- Saucy/Saucy.cs
+++ Saucy/Saucy.cs
@@ -116,7 +116,6 @@ public sealed class Saucy : IDalamudPlugin
         Svc.Framework.Update -= RunBot;
         lock (_lockObj) { DisposeAudio(); }
         CufModule.FuncHook?.Dispose();
-        LimbManager.Dispose();
         ModuleManager.Dispose();
         ECommonsMain.Dispose(); //Don't forget!
         P = null!; //necessary to free the reference for GC
@@ -323,11 +322,13 @@ public sealed class Saucy : IDalamudPlugin
                 var values = stackalloc AtkValue[2];
                 values[0] = new()
                 {
-                    Type = AtkValueType.Int, Int = 0
+                    Type = AtkValueType.Int,
+                    Int = 0
                 };
                 values[1] = new()
                 {
-                    Type = AtkValueType.UInt, UInt = 1
+                    Type = AtkValueType.UInt,
+                    UInt = 1
                 };
                 addon->FireCallback(2, values);
             }
