18f9c9b7b736e63206c60c18278e4fbb56e141c7
Kage <95875972+Kagekazu@users.noreply.github.com>
2026-06-11 02:48:43 +0200

    more cleanup

--- Saucy/Core/Stats.cs
+++ Saucy/Core/Stats.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace Saucy;
 
 public class Stats
@@ -34,4 +36,75 @@ public class Stats
     public int MGPWon = 0;
 
     public Dictionary<string, int> NPCsPlayed = [];
+}
+
+public static class StatsBonusHelper
+{
+    public static int ApplyMgpBonus(int numMgp)
+    {
+        double multiplier = 1;
+        var localPlayer = Svc.Objects.LocalPlayer;
+        if (localPlayer == null)
+        {
+            return numMgp;
+        }
+
+        var jackpot = localPlayer.StatusList.FirstOrDefault(x => x.StatusId == 902);
+        if (jackpot != null)
+        {
+            multiplier += (double)jackpot.Param / 100;
+        }
+
+        if (localPlayer.StatusList.Any(x => x.StatusId == 1079))
+        {
+            multiplier += 0.15;
+        }
+
+        return (int)Math.Round(Math.Ceiling(numMgp * multiplier), 0);
+    }
+}
+
+internal static class StatsSessionClock
+{
+    private static DateTime? triadSinceUtc;
+    private static DateTime? cuffSinceUtc;
+    private static DateTime? limbSinceUtc;
+    private static DateTime? airForceSinceUtc;
+
+    public static void MarkTriadActive() => Mark(ref triadSinceUtc);
+
+    public static void MarkCuffActive() => Mark(ref cuffSinceUtc);
+
+    public static void MarkLimbActive() => Mark(ref limbSinceUtc);
+
+    public static void MarkAirForceActive() => Mark(ref airForceSinceUtc);
+
+    public static void ResetAll()
+    {
+        triadSinceUtc = null;
+        cuffSinceUtc = null;
+        limbSinceUtc = null;
+        airForceSinceUtc = null;
+    }
+
+    public static double GetTriadElapsedHours() => GetElapsedHours(triadSinceUtc);
+
+    public static double GetCuffElapsedHours() => GetElapsedHours(cuffSinceUtc);
+
+    public static double GetLimbElapsedHours() => GetElapsedHours(limbSinceUtc);
+
+    public static double GetAirForceElapsedHours() => GetElapsedHours(airForceSinceUtc);
+
+    private static void Mark(ref DateTime? sinceUtc) => sinceUtc ??= DateTime.UtcNow;
+
+    private static double GetElapsedHours(DateTime? sinceUtc)
+    {
+        var start = sinceUtc ?? C.SessionStartTime;
+        if (start == default || start.Year < 2020 || start > DateTime.UtcNow)
+        {
+            start = DateTime.UtcNow;
+        }
+
+        return Math.Max((DateTime.UtcNow - start).TotalHours, 1.0 / 60.0);
+    }
 }
--- Saucy/Games/OutOnALimb/LimbTypes.cs
+++ Saucy/Games/OutOnALimb/LimbTypes.cs
@@ -1,5 +1,15 @@
+using System;
 using System.Reflection;
 namespace Saucy.OutOnALimb;
+
+[Serializable]
+[Obfuscation(Exclude = true)]
+public class LimbConfig
+{
+    public LimbDifficulty LimbDifficulty = LimbDifficulty.Titan;
+    public int MinSecondsForAnotherRound = 12;
+    public int Step = 10;
+}
 
 public enum LimbDifficulty
 {
